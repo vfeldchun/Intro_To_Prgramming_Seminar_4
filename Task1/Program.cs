47b12ab7e318048126a06b318636ba2ad22a8b49
vfeldchun <vladuz@mail.ru>
2024-02-17 16:23:19 +0300

    Solution for Task1 added!

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,47 +3,56 @@
 // Программа завершается при вводе символа ‘q’ или при вводе числа, 
 // сумма цифр которого чётная.
 
-string? numberString = "";
-
-while (true) 
+class Programm
 {
-    Console.Write("Введите целое число: ");
-    numberString = Console.ReadLine();
-    if (numberString == "q" || IsNumbersSumEven(numberString)) break;
-}
-
-bool IsNumbersSumEven(string? numberString)
-{
-    int number;
-
-    try
+    static void Main()
     {
-        number = Convert.ToInt32(numberString);
+
+        string? numberString = "";
+
+        while (true) 
+        {
+            Console.Write("Введите целое число или нажмите 'q': ");
+            numberString = Console.ReadLine();
+            if (numberString == "q" || IsNumbersSumEven(numberString)) break;
+        }
+
+    
+        bool IsNumbersSumEven(string? numberString)
+        {
+            int number;
+
+            try
+            {
+                number = Convert.ToInt32(numberString);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка в вводе целого числа");
+                return false;
+            }
+
+            if (number < 0) number = -number;
+
+            // Исходя из условия задачи число должно быть минимум двухзначным
+            if (number > 9)
+            {
+                int sum = GetNumberElemantSum(number.ToString());
+                if (sum % 2 == 0) return true;
+            }
+
+            return false;
+        }
+
+        int GetNumberElemantSum(string numberString)
+        {
+            int sum = 0;
+            foreach(var digit in numberString)
+            {
+                sum = sum + Convert.ToInt32(digit);
+            }
+
+            return sum;
+        }
     }
-    catch
-    {
-        Console.WriteLine("Ошибка в вводе целого числа");
-        return false;
-    }
-
-    if (number < 0) number = -number;
-
-    if (number > 9)
-    {
-        int sum = GetNumberElemantSum(number.ToString());
-        if (sum % 2 == 0) return true;
-    }
-
-    return false;
-}
-
-int GetNumberElemantSum(string numberString)
-{
-    int sum = 0;
-    foreach(var digit in numberString)
-    {
-        sum = sum + Convert.ToInt32(digit);
-    }
-
-    return sum;
 }
