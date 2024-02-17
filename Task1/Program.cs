b10cd57e20285eaf3931907007ae222f8cbc1d94
vfeldchun <vladuz@mail.ru>
2024-02-17 16:10:31 +0300

    Solution for Task1 added!

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,48 @@
 // Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, 
 // сумма цифр которого чётная.
+
+string? numberString = "";
+
+while (true) 
+{
+    Console.Write("Введите целое число: ");
+    numberString = Console.ReadLine();
+    if (numberString == "q" || IsNumbersSumEven(numberString)) break;
+}
+
+bool IsNumbersSumEven(string? numberString)
+{
+    int number;
+
+    try
+    {
+        number = Convert.ToInt32(numberString);
+    }
+    catch
+    {
+        Console.WriteLine("Ошибка в вводе целого числа");
+        return false;
+    }
+
+    if (number < 0) number = -number;
+
+    if (number > 9)
+    {
+        int sum = GetNumberElemantSum(number.ToString());
+        if (sum % 2 == 0) return true;
+    }
+
+    return false;
+}
+
+int GetNumberElemantSum(string numberString)
+{
+    int sum = 0;
+    foreach(var digit in numberString)
+    {
+        sum = sum + Convert.ToInt32(digit);
+    }
+
+    return sum;
+}
